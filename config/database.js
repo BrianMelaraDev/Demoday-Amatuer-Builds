b8f75444f7ac59cb5badb08baeaee4299ccc4958
Brian Melara <77566915+BrianMelaraDev@users.noreply.github.com>
2021-05-04 16:54:24 -0400

    Delete config directory

--- config/database.js
+++
@@ -1,7 +0,0 @@
-// config/database.js
-//we are exporting this object so when required we can fetch this object
-module.exports = {
-
-    'url' : 'mongodb+srv://brin:bananaclip@cluster0.afamw.mongodb.net/DEMODAY?retryWrites=true&w=majority', 
-    'dbName': 'demo'
-};
